da8f07d62dca2205695b7be02aeea0daedd7a904
vik <rthvkanand@gmail.com>
2025-08-02 18:50:03 +0530

    basic CSS; still a few error

--- my-react-template/src/App.jsx
+++ my-react-template/src/App.jsx
@@ -3,14 +3,14 @@ import './App.css'
 
 function App() {
   return(
-    <div className='Calculator grid'>
+    <div className='calculator-grid'>
       <div className='output'>
-        <div className='previous-operand'></div>
-        <div className='current-operand'></div>
+        <div className='previous-operand'>IM DISCIPLINED</div>
+        <div className='current-operand'>IM THE BEST</div>
         
       </div>
       <button className='span-two'>AC</button>
-      <button>DEL</button>
+      <button>DEL  </button>
       <button>÷</button>
       <button>1</button>
       <button>2</button>
